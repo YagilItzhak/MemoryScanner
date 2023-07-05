98a27e8e3e50b9ca387661edf0144433821d6fa8
Yagil Itzhak <anagrama.yagil@gmail.com>
2023-07-05 18:49:17 +0300

    use MAX_PATH macro

--- MemoryScanner/main.cpp
+++ MemoryScanner/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include "MemoryScannner.h"
 
-constexpr size_t MAX_PATH_LENGTH = 260;
-constexpr size_t MAX_OPERWTION_LENGTH = 6;
 
 int main(void)
 {
-	wchar_t path[260];
+	wchar_t path[MAX_PATH];
 	unsigned long long int value;
 
 	puts("Please enter the prossess you what to follow:");
-	std::wcin.getline(path, 260);
+	std::wcin.getline(path, MAX_PATH);
 	puts("Please enter the value: ");
 	std::cin >> value;
 
