fb177d59602c427b8b3b038e3df2ea446b3bc93c
Yagil Itzhak <anagrama.yagil@gmail.com>
2023-07-06 13:39:00 +0300

    menu

--- MemoryScanner/MemoryScannner.h
+++ MemoryScanner/MemoryScannner.h
@@ -11,6 +11,7 @@ public:
 
 	void search(const int value);
 	void filter(const int value);
+	void write(const int value);
 	void print(void) const;
 
 private:
@@ -19,10 +20,8 @@ private:
 	void searchProcess(HANDLE process, const int value);
 	void searchMemoryRegion(HANDLE process, const MEMORY_BASIC_INFORMATION& memoryInfo, const int value);
 
-	void filter(const int value);
 	void filterProcess(HANDLE process, const int value);
 
-	void write(const int value);
 	void writeProcess(const HANDLE process, const int value);
 
 private:
--- MemoryScanner/main.cpp
+++ MemoryScanner/main.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
 #include "MemoryScannner.h"
 
+int askForValue(void)
+{
+	int value;
+	fputs("Please enter the value: ", stdout);
+	std::cin >> value;
+	return value;
+}
+
 
 int main(void)
 {
 	wchar_t path[MAX_PATH];
-	unsigned long long int value;
+	int value;
+	char option;
 
-	puts("Please enter the prossess you what to follow:");
+	fputs("Please enter the program you what to follow:", stdout);
 	std::wcin.getline(path, MAX_PATH);
-	puts("Please enter the value: ");
-	std::cin >> value;
 
 	MemoryScannner ms(path);
+
+	value = askForValue();
+
 	ms.search(value);
 	ms.print();
+
+	do
+	{
+		puts("Please enter u to enter an updated value");
+		puts("             w to write a value");
+		puts("             e to exit");
+
+		option = getchar();
+
+		if (option == 'u')
+		{
+			value = askForValue();
+			ms.filter(value);
+			ms.print();
+		}
+		else if (option == 'w')
+		{
+			value = askForValue();
+			ms.write(value);
+		}
+
+
+	} while (option != 'e');
 	
 	return 0;
 }
